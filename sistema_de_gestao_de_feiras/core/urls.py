ba7984bb180fcdadfa66de581a70acdf6744e23b
jm-236 <jaowjjw@gmail.com>
2025-06-16 19:50:53 -0300

    feat: Criação da tela de login.

--- sistema_de_gestao_de_feiras/core/urls.py
+++ sistema_de_gestao_de_feiras/core/urls.py
@@ -2,10 +2,12 @@
 
 from django.urls import path
 from . import views  # Importa as views do app core
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Quando a URL for vazia (''), chame a view 'pagina_inicial'
     path('', views.pagina_inicial, name='inicio'),
 	path('feira/<int:feira_id>', views.detalhes_feira, name='detalhes_feira'),
     path('expositor/<int:expositor_id>/', views.detalhes_expositor, name='detalhes_expositor'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
 ]
